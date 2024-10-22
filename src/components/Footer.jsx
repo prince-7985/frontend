5df8a6aba3271aa5b6e10ea1201041ab1aec6e60
prince sharma <princesharma798505@gmail.com>
2024-10-22 23:12:58 +0530

    initial

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">Prince Restaurant</div>
+          <div className="right">
+            <p>Greater Noida,Uttar Pradesh</p>
+            <p>Open: 10:00 A.M - 11:00 PM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By PRINCESHARMA</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By &copy; princesharma@gmail.com</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
